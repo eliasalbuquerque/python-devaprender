1b8f370b651bf65419ae6f980e75f3caf97671a4
Elias Albuquerque <78819295+eliasalbuquerque@users.noreply.github.com>
2023-12-06 20:36:55 -0300

    feat: finish project 2 mini-game turtle
    ADDED FEATURES:
    * functions print manual and clear terminal
    * list of directions
    * loop while to request input
    * input control
    * logical to direction the turtle and set the distance
    * inittial settings to turn the arrow in a green turtle

--- mini-game.py
+++ mini-game.py
@@ -1,3 +1,10 @@
+# title: 'mini-game.py'
+# author: 'Elias Albuquerque'
+# version: 'Python 3.12.0'
+# created: '2023-12-05'
+# update: '2023-12-06'
+
+
 # Desafio 1
 # Monte um mini-game turtle, que possibilite que o usuário controle para qual
 # direção a tartaruga deve andar(frente/trás) e qual ângulo deverá ser tomado a
@@ -9,47 +16,6 @@
 # totalmente através do input do usuário
 
 
-# Dicas Iniciais
-# * Crie uma nova turtle primeiro
-# * Coloca seu programa em loop
-# * Faça perguntas ao usuário para decidir se a tartaruga deve movimentar para 
-#   frente ou para trás
-# * Após decidir se ele deve movimentar para frente ou para trás, receba do 
-#   usuário quantos pixels devem ser percorridos
-# * Faça perguntas ao usuário para decidir se a tartaruga deve rotacionar para 
-#   esquerda ou direta
-# * Após decidir se ele deve rotacionar para esquerda ou direita, receba do 
-#   usuário quantos pixels devem ser rotacionados
-# * Ao executar essa ação pergunte ao usuário "Continuar andando?", e reaga de 
-#   acordo com a resposta do usuário.
-
-# Dicas Adicionais
-# * Não esqueça de converter o input do usuário para o tipo apropriado
-# * Resolva um problema de cada vez e lembre de seguir a seguinte lógica:
-# Pergunte -> Processe resposta -> A
-
-# import turtle as t
-# from random import random
-
-# for i in range(100):
-#     steps = int(random() * 100)
-#     angle = int(random() * 360)
-#     t.right(angle)
-#     t.fd(steps)
-
-# from turtle import Turtle
-# from random import random
-
-# t = Turtle()
-# for i in range(100):
-#     steps = int(random() * 100)
-#     angle = int(random() * 360)
-#     t.right(angle)
-#     t.fd(steps)
-
-# t.screen.mainloop()
-
-# Importing turtle module
 import turtle
 import os
 
@@ -61,38 +27,54 @@ t.speed(1)
 t.shape("turtle")
 t.color("green")
 
-# manual do jogo
-print('MOVIMENTAÇÃO:')
-print('Direção:\n\ta = para trás\n\ts = para baixo\n\td = para frente\n\tw = para cima')
-print('Distância: de 1 a 10') 
-print('Exemplo:\n\tInserir direção e distância: a10')
+# funcao manual do jogo
+def manual_jogo():
+    print('----- MANUAL -----')
+    print('Direção: a = para esquerda\n\t s = para baixo\n\t d = para direita\n\t w = para cima')
+    print('Distância: de 1 a 10') 
+    print('Exemplo: Inserir direção e distância: d10  --> direita 10 passos\n')
 
-# movimentacao
+# funcao clear the terminal
+def clear_terminal():
+    command = 'cls' if os.name == 'nt' else 'clear'
+    os.system(command)
+
+# limpa o terminal e mostra o manual
+clear_terminal()
+manual_jogo()
+
+# movimentacao da turtle
+direcoes = ['a','s','d','w']
+
 while True:
-    dist = input('Inserir direção e distância: ')
-    t.forward(dist)
+    coordenadas = input('Inserir direção e distância: ')
+    direcao = coordenadas[0].lower()
+    distancia = int(coordenadas[1:] + '0')
 
+    if direcao in direcoes and distancia <= 100:
 
-# t.forward(100)
-# t.right(90)
-# t.forward(100)
+        # movimentar para esquerda 180°
+        if direcao == 'a':
+            t.seth(180)
+            t.forward(distancia)
 
-# Hiding the turtle
-# t.hideturtle()
+        # movimentar para baixo 270°
+        if direcao == 's':
+            t.seth(270)
+            t.forward(distancia)
 
+        # movimentar para direita 0°
+        if direcao == 'd':
+            t.seth(0)
+            t.forward(distancia)
 
-# ------------- SPACE TO FINISH APP ----------------
-# Function to close the window when space key is pressed
-# def close_window():
-    # turtle.bye()
+        # movimentar para cima 90°
+        if direcao == 'w':
+            t.seth(90)
+            t.forward(distancia)
 
-# Binding the space key to the close_window function
-# turtle.onkey(close_window, "space")
+    else:
+        print('\n> Inserir dados conforme MANUAL!')
+        manual_jogo()
 
-# Listening for key presses
-# turtle.listen()
-# ------------- SPACE TO FINISH APP ----------------
-
-
-# Keeping the window open until it is manually closed or space key is pressed
 turtle.mainloop()
