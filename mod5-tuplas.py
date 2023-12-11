2a7754898fe4f152b2d0261043270d9452ffc803
Elias Albuquerque <78819295+eliasalbuquerque@users.noreply.github.com>
2023-12-11 17:55:31 -0300

    feat: add tuplas
    ADDED FEATURES
    
    * create tuple
    * joining tuples

---
+++ mod5-tuplas.py
@@ -0,0 +1,16 @@
+# 202312 - Python 3.12.0
+# 5.7 - Tuplas
+
+
+# 1. criar tupla
+sites = ('youtube.com', 'facebook.com', 'instagram.com')
+print(f'1. {sites[1]}')
+
+# 2. unindo tuplas
+empresas = ('google', 'meta', 'amazon')
+nova_tupla = sites + empresas
+print(f'2. {nova_tupla}')
+
+
+# NOTE:
+# Nao é possivel alterar, adicionar e remover elementos de uma Tupla.
