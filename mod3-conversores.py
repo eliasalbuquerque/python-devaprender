e07847ccfcd903dc5efb867bbaa5cfcfb5a22d1c
Elias Albuquerque <78819295+eliasalbuquerque@users.noreply.github.com>
2023-12-15 12:50:02 -0300

    update: add converting collections
    UPDATE
    
    * converting list into dictionaries and tuples
    * including keys in the dictionary

--- mod3-conversores.py
+++ mod3-conversores.py
@@ -1,25 +1,48 @@
 # 202311 - Python 3.12.0
 # 3 - Aprenda a controlar o fluxo de seus programas
+# 3.6 - Convertendo entre tipos                 --> ln: 7
+# 5.16 - Conversão entre tipos (coleções)       --> ln: 32
+
+
 # 3.6 - Convertendo entre tipos
 
 
+# 1. convertendo e indentificando os tipos
 a = 5
-print(f'int: {a} {type(a)}') #5 <class 'int'>
+print(f'1. int: {a} {type(a)}')  # 5 <class 'int'>
 
 a = float(a)
-print(f'float: {a} {type(a)}') #5.0 <class 'float'>
+print(f'2. float: {a} {type(a)}')  # 5.0 <class 'float'>
 
 a = str(a)
-print(f'str: {a} {type(a)}') #'5.0' <class 'str'>
+print(f'3. str: {a} {type(a)}')  # '5.0' <class 'str'>
 
+
+# 4. convertendo float em lista
+str_list = list(a)
+print(f'\n4. list: {str_list}  {type(str_list)}')
+
+# 5. convertendo float em tupla
+str_tuple = tuple(a)
+print(f'5. tuple: {str_tuple}  {type(str_tuple)}')
+
+# 6. convertendo dados em um dicionario
 b = 20
 c = 2.5
 
-str_list = list(a)
-print(f'list: {str_list}  {type(str_list)}')
-
-str_tuple = tuple(a)
-print(f'tuple: {str_tuple}  {type(str_tuple)}')
-
 str_dict = {'string': a, 'inteiro': b, 'float': c}
 print(f'dictionary: {str_dict}  {type(str_dict)}')
+
+
+# 5.16 - Conversão entre tipos (coleções)
+
+
+# 7. convertendo lista em:
+numeros = [10, 20, 30, 40, 50]
+print(f'\n7. dicionario: {set(numeros)}')
+print(f'8. tupla: {tuple(numeros)}')
+
+# 9. incluindo chaves no dicionario:
+dicionario = {
+    i: num for i, num in enumerate(numeros, 1)}
+print(f'\n9. dicionario (keys): {dicionario}')
